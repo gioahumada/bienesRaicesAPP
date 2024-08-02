885ae2954d62b5501edcbdefb9444186b3bc2708
Giovanni Ahumada <giovanni.ahumada.t@gmail.com>
2024-08-01 21:55:29 -0400

    chore: Import admin styles and add error alert component

---
+++ includes/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+function conectarDB() {
+    $db = mysqli_connect('localhost', 'root', '', 'bienesraices_crud');
+    if(!$db) {
+        echo "Error no se pudo conectar";
+        exit;
+    } 
+
+    return $db;
+}
